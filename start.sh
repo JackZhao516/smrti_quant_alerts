05f260eeb5d2ecca7e25a6b196f5fb78a913c87a
Jack Zhao <jackzhao516@utexas.edu>
2024-04-08 00:05:46 -0700

    update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-nohup python3 -m smrti_quant_alerts.main "$1" > "$1.log" 2>&1 &
+[ ! -d "runtime_logs" ] && mkdir -p "runtime_logs"
+nohup python3 -m smrti_quant_alerts.main "$1" > "runtime_logs/$1.log" 2>&1 &
