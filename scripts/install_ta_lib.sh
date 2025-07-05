219db5149316813c977429d1fcbe02d803c25fbf
Jack Zhao <jiazhenz@umich.edu>
2025-07-05 15:17:32 -0700

    Update install_ta_lib.sh

--- scripts/install_ta_lib.sh
+++ scripts/install_ta_lib.sh
@@ -1,8 +1,9 @@
-git clone https://github.com/JackZhao516/ta-lib-0.4.0-src
-cd ta-lib-0.4.0-src
-chmod +x configure
-./configure --prefix=/usr
-make
-sudo make install
+sudo apt-get update
+sudo apt-get install -y build-essential wget git cmake
+git clone  https://github.com/ta-lib/ta-lib.git ta-lib
+cd ta-lib
+chmod +x install uninstall
+sudo ./uninstall --prefix /usr
+sudo ./install --prefix /usr
 cd ..
-rm -rf ta-lib-0.4.0-src
+rm -rf ta-lib
